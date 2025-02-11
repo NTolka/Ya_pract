0e544503b4ac26c11a6c41645505463a3d69dc7d
Tolka <Bruckbond13@gmail.com>
2025-02-11 14:26:07 +0500

    func Contains(x[]string,a string)bool

--- mathxx.go
+++ mathxx.go
@@ -3,3 +3,12 @@ package yapract
 func Add(a, b int) int {
 	return a + b
 }
+
+func Contains(x []string, a string) bool {
+	for _, v := range x {
+		if a == v {
+			return true
+		}
+	}
+	return false
+}
